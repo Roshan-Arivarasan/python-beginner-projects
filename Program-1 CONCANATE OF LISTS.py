d91ad261fa78ff21a426f04e1f7fa41d65bae7e1
Roshan-Arivarasan <roshan.arivarasan@gmail.com>
2026-04-25 17:08:06 +0530

    Add files via upload

---
+++ Program-1 CONCANATE OF LISTS.py
@@ -0,0 +1,7 @@
+L1=eval(input("Enter the number of elements:"))
+L2=eval(input("Enter the number of elements:"))
+print("The length of L1 is",len(L1))
+print("The length of L2 is",len(L2))
+L3=L1+L2
+print("Merged list",L3)
+print("The length of L3 is",len(L3))
